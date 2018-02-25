bf50e571fb1499541fe91a799c557846c8778324
Alex Hermansson <grobgeld@kth.se>
2018-02-25 16:40:16 +0100

    shit keras

--- mackey_glass.py
+++ mackey_glass.py
@@ -17,23 +17,23 @@ for i, t in enumerate(range(301, 1501)):
     X[i] = np.array([x[t-20], x[t-15], x[t-10], x[t-5], x[t]])
     T[i] = x[t+5]
 
-sigma=1
+sigma=0.03
 
 test_size = 200
 X_test = X[-test_size:]
 X_train = X[:-test_size]
 T_test = T[-test_size:]
 T_train = T[:-test_size]
-T_train+=np.random.normal(0,sigma, T_train.shape)
+T_train_noise = T_train + np.random.normal(0,sigma, T_train.shape)
 
-hidden_first = 150
+hidden_first = 15
 hidden_second = 10
 d = 5
 M = 1
 epochs = 5000
 b_size = X_train.shape[0]
 b_size_test=X_test.shape[0]
-lambd=0.01
+lambd=0.000001
 
 # two layer perceptron
 model = Sequential()
@@ -43,30 +43,32 @@ model.add(Dense(hidden_first, input_shape=(d,), activation='relu', kernel_regula
 model.add(Dense(M))
 sgd = optimizers.SGD(lr=0.01, momentum=0.9) # add learning rate decay or nesterov momentum?
 model.compile(optimizer=sgd, loss='mean_squared_error')
-history = model.fit(X_train, T_train, batch_size=b_size, epochs=epochs, callbacks=[EarlyStopping(patience = 20)], verbose=0, validation_split=0.4)
+history = model.fit(X_train, T_train_noise, batch_size=b_size, epochs=epochs, verbose=0)#, callbacks=[EarlyStopping(patience = 20)], validation_split=0.4)
 #history=model.fit(X_train, T_train, batch_size=b_size, epochs=epochs, verbose=0,validation_split=0.4)
 
 plt.plot(history.history['loss'],c="r")
-plt.plot(history.history['val_loss'],c="b")
+#plt.plot(history.history['val_loss'],c="b")
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 #plt.axis([0, epochs,0,1])
-plt.legend(['train', 'val'])
+#plt.legend(['train', 'val'])
+plt.legend('train')
 plt.show()
 
-y = model.predict(X_test, verbose=2, batch_size=b_size_test)
+y = model.predict(X_train, verbose=2, batch_size=b_size)
 t = np.arange(y.size)
 
-test_MSE = model.evaluate(X_test, T_test, verbose = 0, batch_size=b_size_test)
+test_MSE = model.evaluate(X_train, T_train, verbose = 0, batch_size=b_size)
 print('test error: ', test_MSE)
 
-prediction=model.predict(X, verbose=2, batch_size=b_size_test)
+prediction=model.predict(X_train, verbose=2, batch_size=b_size_test)
 t_prediction=np.arange(prediction.size)
 
 plt.plot(t, y, 'r', label = 'Predicted series')
-plt.plot(t, T_test, 'b', label = 'Real series')
+plt.plot(t, T_train, 'b', label = 'Real series')
 plt.legend()
+#plt.axis([400, 600, -2.5, 3])
 plt.show()
 
 '''plt.plot(t_prediction, prediction, 'r', label = 'Predicted series')
